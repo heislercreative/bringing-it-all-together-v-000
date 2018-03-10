7ac46a99108a2b7aa6315c6380018e0e17199675
Brennan Heisler <learn@heislercreative.com>
2018-03-10 23:59:48 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -38,7 +38,8 @@ class Dog
   
   def self.find_or_create_by(name:, breed:)
     dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name: name, breed: breed)
-    
+    if !dog.empty?
+      dog = self.new
   end
   
   def self.new_from_db(row)
