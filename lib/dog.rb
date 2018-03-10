6c1da1cd11bb969b529eb703d946b9eb2ec9d20e
Brennan Heisler <learn@heislercreative.com>
2018-03-10 23:11:47 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,7 +2,7 @@ def Dog
   attr_accessor :name, :breed
   attr_writer :id
   
-  def initialize(name, breed, id=nil)
+  def initialize(name:, breed:, id: nil)
     @id = id
     @name = name
     @breed = breed
