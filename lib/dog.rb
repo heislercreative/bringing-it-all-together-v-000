3de3d345f3547224c9ba6f4743637d6ea478658d
Brennan Heisler <learn@heislercreative.com>
2018-03-10 23:28:55 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -34,6 +34,17 @@ class Dog
     end.first
   end
   
+  def self.find_by_id(id)
+    sql = <<-SQL
+      SELECT * FROM dogs
+      WHERE id = ?
+      LIMIT 1
+    SQL
+    DB[:conn].execute(sql, id).collect do |row|
+      self.new_from_db(row)
+    end.first
+  end
+  
   def self.create_table
     sql = <<-SQL
       CREATE TABLE IF NOT EXISTS dogs (
